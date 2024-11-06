c4eba1d43307962124bdf462fc58829768494b98
B00ttl3 <mumair3825@gmail.com>
2024-11-07 00:57:42 +0500

    made some user based data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,11 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title: string = 'This loaded dynamically'
   
-  
+  user: Array<any> = [
+    {name: 'Umair', age: 26, email: 'umair@gmail.com'},
+    {name: 'Babar', age: 24, email: 'babar@gmail.com'},
+    {name: 'Kamran', age: 29, email: 'kamran@gmail.com'},
+    {name: 'Rehan', age: 23, email: 'rehan@gmail.com'},
+  ]
+
 }
