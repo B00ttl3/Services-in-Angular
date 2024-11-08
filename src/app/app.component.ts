997b96a37e7c0dbd1e814b74be16aa1adcbf4ac5
B00ttl3 <mumair3825@gmail.com>
2024-11-09 01:34:33 +0500

    Learnt about how to render data from another child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { PostsListComponent } from './posts-list/posts-list.component';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent, PostsListComponent],
+  imports: [RouterOutlet, UserComponent, PostsListComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title: string = 'This loaded dynamically'
-  
-  users: Array<any> = [
-    {name: 'Umair', age: 26, email: 'umair@gmail.com'},
-    {name: 'Babar', age: 24, email: 'babar@gmail.com'},
-    {name: 'Kamran', age: 29, email: 'kamran@gmail.com'},
-    {name: 'Rehan', age: 23, email: 'rehan@gmail.com'},
-  ]
 
+  userService: any;
+
+  constructor() {
+    this.userService = new UserService();
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,9 @@
 
 class UserService {
-    
+    users: Array<any> = [
+        {name: 'Umair', age: 26, email: 'umair@gmail.com'},
+        {name: 'Babar', age: 24, email: 'babar@gmail.com'},
+        {name: 'Kamran', age: 29, email: 'kamran@gmail.com'},
+        {name: 'Rehan', age: 23, email: 'rehan@gmail.com'},
+      ]
 }
