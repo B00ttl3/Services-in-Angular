973462af855c0fc5b628cfc0419a7bbddbe6dfad
B00ttl3 <mumair3825@gmail.com>
2024-11-19 01:01:08 +0500

    added the info about how to make the service injectable

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from "@angular/core";
   providedIn: 'root'
 })
 
+// to mkae it injectable we first will add the @Injectable and select it to auto complete it with imports. and then we will makea paramter inside the @Injectable useing the {which is providedIn: 'root'} and we set it's value to root becasue we are saying to inject this service at the root level of our application.
+
 export class UserService {
     users: Array<any> = [
         {name: 'Umair', age: 26, email: 'umair@gmail.com'},
