b5674aa8ed09a7534cf7cea8f6216e00e79357f6
B00ttl3 <mumair3825@gmail.com>
2024-11-23 02:47:32 +0500

    Added the interface which helps to prevent making typos or mistakes

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 
+// we use interface so that it can help us to be safe from the typos which we can make insdie our code, so now as we have added the interface with the name (Post in this case) and after that we have added the details which we want insid our array, which are id and its type and same with title and post, so if we make any typo or put any other data type instead of what we have defined in interface. it will give us error on the spot which will help us to correct the mistake on the spot.
+
+interface Post {
+  id: number,
+  title: string,
+  post: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  posts: Array<any> = [
+
+  // the Post inside the <> is the Post from the interface above, we used it here si that we don't make any typos in our array.
+
+  posts: Array<Post> = [
     {id: 1, title: 'Post Title 1', post: 'Dummy Post 1'},
     {id: 2, title: 'Post Title 2', post: 'Dummy Post 2'},
     {id: 3, title: 'Post Title 3', post: 'Dummy Post 3'},
