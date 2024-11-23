de399dadd628ed6c366dc54e31c749a01446e304
B00ttl3 <mumair3825@gmail.com>
2024-11-24 00:38:56 +0500

    Learnt about Optional field in interface

--- src/app/interfaces/post.ts
+++ src/app/interfaces/post.ts
@@ -1,5 +1,9 @@
 export interface Post {
     id: number,
   title: string,
-  post: string
+  post: string,
+  username?: string
 }
+
+
+// The ? after the username represents the Optional Field. Means its the users choice whether they want to add their name or not. by using this we are not making our previous arrays code wrong as we didn't add the username field there. but in here now if we want to get the username we can just type ? after that and it will become optional for the users and for us to use it in anyother code if we want to
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { JsonPipe } from '@angular/common';
 import { PostService } from '../services/post.service';
+import { Post } from '../interfaces/post';
 
 @Component({
   selector: 'app-user',
@@ -19,7 +20,7 @@ export class UserComponent {
   }
 
   addPost() {
-    let postData = {
+    let postData:Post = {
       id: 8,
       title: 'Post Title 8',
       post: 'dummy post 8'
